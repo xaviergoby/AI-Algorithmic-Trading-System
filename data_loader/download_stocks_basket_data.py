2452abd1637b3397c77bd1cf7adb4cb55e21c7e1
Unknown <xgoby@hotmail.com>
2019-02-20 03:16:34 +0100

    added ability to fetch, save and load a basket of multiple stocks data

--- data_loader/download_stocks_basket_data.py
+++ data_loader/download_stocks_basket_data.py
@@ -13,41 +13,47 @@
 # stock_basket_idxs_dict = {"Semiconductors":["INTC", "QCOM", "TXN", "ADI", "XLNX", "ASML", "NVDA", "AMD"]}
 
 from datetime import datetime
-from stock_data import StockData
+from data_loader.stock_data import StockData
 import pandas as pd
 
 
 class StockDataBasket:
 
     def __init__(self, stock_idxs, end_date = datetime.today().strftime("%m-%d-%Y"), start_date = "01-06-2014"):
-        self.stock_idxs = stock_idxs
+        self.industry_specific_stock_idxs = stock_idxs
         self.end_date = end_date
         self.start_date = start_date
-        self.industries = list(self.stock_idxs.keys()) if len(list(self.stock_idxs.keys())) != 1     \
-            else list(self.stock_idxs.keys())[0]
-        self.indexes = list(self.stock_idxs.values())
+        self.industry_name = list(self.industry_specific_stock_idxs.keys()) if len(list(self.industry_specific_stock_idxs.keys())) != 1     \
+            else list(self.industry_specific_stock_idxs.keys())[0]
+        self.industry_specific_stock_idxs_list = self.industry_specific_stock_idxs[self.industry_name]
+        self.file_name = "{0}_stocks_data_{1}_{2}".format(self.industry_name, self.start_date, self.end_date)
 
-    def save_stock_data_to_hdf5(self, stock_data_df, hdf5_file_name, idx):
-        with pd.HDFStore(hdf5_file_name) as store:
-            store.put(idx, stock_data_df)
-
-    def save_data_as_h5_file_(self, industry_name, hdf5_file_name):
-        industry_idxs = self.stock_idxs[industry_name]
+    def save_stock_data_basket_as_h5_file(self):
+        stock_basket_store = pd.HDFStore(self.file_name)
+        industry_idxs = self.industry_specific_stock_idxs_list
         for idx in industry_idxs:
             stock_data_obj = StockData(idx)
             stock_data = stock_data_obj.get_stocks_data()
-            self.save_stock_data_to_hdf5(stock_data, hdf5_file_name, idx)
+            stock_data.to_hdf(stock_basket_store, key=idx)
+        stock_basket_store.close()
+
+    def load_h5_file_stock_data_basket(self):
+        stock_basket_dict = {}
+        stock_basket_store = pd.HDFStore(self.file_name)
+        stock_basket_store_keys = stock_basket_store.keys()
+        for idx_key in stock_basket_store_keys:
+            stock_data = stock_basket_store.get(idx_key)
+            stock_idx = idx_key.strip('/')
+            stock_basket_dict[stock_idx] = stock_data
+        stock_basket_store.close()
+        return stock_basket_dict
 
 
 # if __name__ == "__main__":
 #     stock_basket_idxs_dict = {"Semiconductors": ["INTC", "QCOM", "TXN", "ADI", "XLNX", "ASML", "NVDA", "AMD"]}
 #     x = StockDataBasket(stock_basket_idxs_dict)
-#     file_name = "{0}_stocks_data_{1}_{2}".format(x.industries, x.start_date, x.end_date)
-#     x.save_data_as_h5_file_("Semiconductors", hdf5_file_name = file_name)
-#     data = pd.HDFStore(file_name)
-#     print(data.get(data.keys()[0]))
-#     data.close()
-
+#     x.save_stock_data_basket_as_h5_file()
+#     my_data_dict = x.load_h5_file_stock_data_basket()
 
 
 
